00dfcc451d19068959460a35d1a7244f09b996b6
AntoMei <antonio.meirosu444@gmail.com>
2023-02-15 09:40:13 +0100

    Realizado hasta el punto 2.3, he realizado el team chefs

--- symfony-proyecto-final/src/Controller/PageController.php
+++ symfony-proyecto-final/src/Controller/PageController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    public function teamTemplate(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/index.html.twig', []);
+        $repository = $doctrine->getRepository(Team::class);
+        $team = $repository->findAll();
+        return $this->render('partials/_team.html.twig', 
+            compact('team')
+        );
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        return $this->render('page/index.html.twig');
     }
 
     #[Route('/contact', name: 'contact')]
